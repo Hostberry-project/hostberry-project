da854a9cef9e84cba639dbab50ff3f36d8f8612e
Hostberry-project <aka.kuro@disroot.org>
2026-03-19 19:23:55 +0100

    Auto-subida: 2026-03-19 19:23:55

---
+++ internal/server/start.go
@@ -0,0 +1,62 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"hostberry/internal/config"
+	"hostberry/internal/i18n"
+)
+
+func ServerAddr() string {
+	return fmt.Sprintf("%s:%d", config.AppConfig.Server.Host, config.AppConfig.Server.Port)
+}
+
+// Start levanta HTTPS si está configurado y los ficheros existen, en caso contrario levanta HTTP.
+// Mantiene un shutdown gracioso con señal SIGINT/SIGTERM.
+func Start(app *fiber.App) {
+	addr := ServerAddr()
+
+	i18n.LogTf("logs.server_starting", addr)
+	i18n.LogTf("logs.server_config",
+		config.AppConfig.Server.Debug,
+		config.AppConfig.Server.ReadTimeout,
+		config.AppConfig.Server.WriteTimeout)
+
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt, syscall.SIGTERM)
+		<-sigint
+
+		i18n.LogTln("logs.server_stopping")
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := app.ShutdownWithContext(ctx); err != nil {
+			i18n.LogTf("logs.server_shutdown_error", err)
+		}
+		os.Exit(0)
+	}()
+
+	i18n.LogTf("logs.server_ready", addr)
+
+	if config.AppConfig.Server.TLSCertFile != "" && config.AppConfig.Server.TLSKeyFile != "" {
+		if _, err := os.Stat(config.AppConfig.Server.TLSCertFile); err == nil {
+			if _, err := os.Stat(config.AppConfig.Server.TLSKeyFile); err == nil {
+				if err := app.ListenTLS(addr, config.AppConfig.Server.TLSCertFile, config.AppConfig.Server.TLSKeyFile); err != nil {
+					i18n.LogTfatal("logs.server_start_error", err)
+				}
+				return
+			}
+		}
+	}
+
+	if err := app.Listen(addr); err != nil {
+		i18n.LogTfatal("logs.server_start_error", err)
+	}
+}
+
