2fefe0921c42a098a40e87cd4189f7e3a584ea61
Hostberry-project <aka.kuro@disroot.org>
2026-03-19 19:30:01 +0100

    Auto-subida: 2026-03-19 19:30:01

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"embed"
+
+	"hostberry/internal/config"
+	"hostberry/internal/database"
+	"hostberry/internal/i18n"
+	server "hostberry/internal/server"
+)
+
+var templatesFS embed.FS
+var staticFS embed.FS
+
+func main() {
+	if err := config.Load(); err != nil {
+		i18n.LogTfatal("logs.config_load_error", err)
+	}
+	config.Normalize(i18n.LogTf)
+
+	if err := i18n.Init("locales"); err != nil {
+		i18n.LogTf("logs.i18n_init_warning", err)
+	}
+
+	if err := database.Init(); err != nil {
+		i18n.LogTfatal("logs.db_init_error", err)
+	}
+
+	// Establecer idioma de logs desde configuración del sistema (después de inicializar BD).
+	if configs, err := database.GetAllConfigs(); err == nil {
+		if lang, ok := configs["log_language"]; ok && lang != "" {
+			i18n.SetLogLanguage(lang)
+		}
+	}
+
+	i18n.LogTln("logs.checking_admin")
+	createDefaultAdmin()
+
+	server.StartWiFiAutoConnect()
+
+	app := server.CreateApp(templatesFS, staticFS)
+	server.SetupRoutes(app)
+	server.Start(app)
+}
+
