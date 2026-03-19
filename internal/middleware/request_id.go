28d0d7cf9817d4f8d688facab03503ecae5a8d3f
Hostberry-project <aka.kuro@disroot.org>
2026-03-19 15:31:54 +0100

    Auto-subida: 2026-03-19 15:31:54

---
+++ internal/middleware/request_id.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// RequestIDMiddleware genera y propaga `X-Request-ID`.
+func RequestIDMiddleware(c *fiber.Ctx) error {
+	requestID := c.Get("X-Request-ID")
+
+	if requestID == "" {
+		bytes := make([]byte, 16)
+		if _, err := rand.Read(bytes); err == nil {
+			requestID = hex.EncodeToString(bytes)
+		} else {
+			requestID = generateSimpleID()
+		}
+	}
+
+	c.Locals("request_id", requestID)
+	c.Set("X-Request-ID", requestID)
+
+	return c.Next()
+}
+
+func generateSimpleID() string {
+	// Fallback cuando rand.Read falla: tiempo + pid para reducir colisiones.
+	b := make([]byte, 8)
+	for i := range b {
+		b[i] = byte(time.Now().UnixNano() >> (i * 8))
+	}
+	return hex.EncodeToString(b)
+}
+
