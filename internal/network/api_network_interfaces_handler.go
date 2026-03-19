339ed47ed00ddf61a61c945ca6f198466aaadbd5
Hostberry-project <aka.kuro@disroot.org>
2026-03-20 00:19:25 +0100

    Auto-subida: 2026-03-20 00:19:25

--- internal/network/api_network_interfaces_handler.go
+++ internal/network/api_network_interfaces_handler.go
@@ -162,7 +162,7 @@ func NetworkInterfacesHandler(c *fiber.Ctx) error {
 			hostnameCmd := exec.Command("sh", "-c", "hostname -I 2>/dev/null | awk '{print $1}'")
 			if hostnameOut, err := hostnameCmd.Output(); err == nil {
 				hostnameIP := strings.TrimSpace(string(hostnameOut))
-				if hostnameIP != "" {
+				if hostnameIP != "" && validators.ValidateIP(hostnameIP) == nil {
 					checkCmd := exec.Command("sh", "-c", fmt.Sprintf("ip addr show %s 2>/dev/null | grep -q '%s' && echo '%s'", ifaceName, hostnameIP, hostnameIP))
 					if checkOut, err := checkCmd.Output(); err == nil {
 						checkIP := strings.TrimSpace(string(checkOut))
