4c4523128c6c5e95076bf07d6919dff3618918aa
Hostberry-project <aka.kuro@disroot.org>
2026-03-19 10:32:59 +0100

    Auto-subida: 2026-03-19 10:32:59

--- health.go
+++ health.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"runtime"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -89,4 +91,34 @@ func livenessCheckHandler(c *fiber.Ctx) error {
 		"status":  "alive",
 		"message": "Application is running",
 	})
+}
+
+// metricsHandler expone métricas sencillas en texto plano (formato tipo Prometheus).
+// Está pensado para ser consumido por Prometheus o por scripts de monitoreo.
+func metricsHandler(c *fiber.Ctx) error {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+
+	// NOTA: no exponemos información sensible (ni usuarios, ni tokens, etc.).
+	now := time.Now().Unix()
+
+	body := "" +
+		"# HELP hostberry_up 1 si la aplicación está respondiendo.\n" +
+		"# TYPE hostberry_up gauge\n" +
+		"hostberry_up 1\n\n" +
+		"# HELP hostberry_build_info Información básica de la build (versión estática y runtime).\n" +
+		"# TYPE hostberry_build_info gauge\n" +
+		"hostberry_build_info{version=\"2.0.0\",go_version=\"" + runtime.Version() + "\"} 1\n\n" +
+		"# HELP hostberry_mem_bytes Uso de memoria total reportado por Go (bytes).\n" +
+		"# TYPE hostberry_mem_bytes gauge\n" +
+		"hostberry_mem_bytes " + fmt.Sprintf("%d", m.Alloc) + "\n\n" +
+		"# HELP hostberry_goroutines Número de goroutines actuales.\n" +
+		"# TYPE hostberry_goroutines gauge\n" +
+		"hostberry_goroutines " + fmt.Sprintf("%d", runtime.NumGoroutine()) + "\n\n" +
+		"# HELP hostberry_unix_time_seconds Marca de tiempo UNIX del último scrape.\n" +
+		"# TYPE hostberry_unix_time_seconds gauge\n" +
+		"hostberry_unix_time_seconds " + fmt.Sprintf("%d", now) + "\n"
+
+	c.Set(fiber.HeaderContentType, "text/plain; charset=utf-8")
+	return c.SendString(body)
 }
